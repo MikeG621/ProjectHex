6fda352949c6ebd9ed5afefc39c0204c7f6f443b
Mike <mjgaisser@gmail.com>
2015-07-04 15:19:15 -0500

    updates

---
+++ footer.js
@@ -0,0 +1,9 @@
+document.write('<h4>\
+<a name="copyright-information" class="anchor" href="#copyright-information"><span class="octicon octicon-link"></span></a>Copyright Information</h4>\
+\
+<p>Copyright © Michael Gaisser, 2012-</p>\
+\
+<p>This software is subject to the terms of the Mozilla Public \
+License, v. 2.0. If a copy of the MPL (License.txt) was not distributed \
+with this file, You can obtain one at <a href="http://mozilla.org/MPL/2.0/">http://mozilla.org/MPL/2.0/</a></p>\
+');
---
+++ header.js
@@ -0,0 +1,12 @@
+document.write('\
+	<div class="container">\
+		<h1>ProjectHex</h1>\
+		<h2>Project-based hex editor</h2>\
+\
+		<section id="downloads">\
+			<a href="https://github.com/MikeG621/ProjectHex/zipball/master" class="btn">Download as .zip</a>\
+			<a href="https://github.com/MikeG621/ProjectHex/tarball/master" class="btn">Download as .tar.gz</a>\
+			<a href="https://github.com/MikeG621/ProjectHex" class="btn btn-github"><span class="icon"></span>View on GitHub</a>\
+		</section>\
+	</div>\
+');
